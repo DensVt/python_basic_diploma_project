bc0fd6ed06851791ff8687f7cca411e3bc0f28b8
Denis Mtv <Freeflop@outlook.com>
2023-03-27 12:23:34 +0300

    help and history added correct error output

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from aiogram.types import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
 from aiohttp import ClientSession
 import asyncio
 import os
+from datetime import datetime
 from config import API_TOKEN, OPENWEATHERMAP_API_KEY
 
 
@@ -87,12 +88,21 @@ def format_forecast(data, days=1):
 
     return forecast_str
 
-
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 dp.middleware.setup(LoggingMiddleware())
+
+
+user_history = {}
+
+def add_to_history(user_id, city):
+    timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    if user_id not in user_history:
+        user_history[user_id] = []
+    user_history[user_id].append((city, timestamp))
+
 
 async def send_forecast_buttons(chat_id, city):
     keyboard = InlineKeyboardMarkup()
@@ -100,9 +110,13 @@ async def send_forecast_buttons(chat_id, city):
         InlineKeyboardButton("Сегодня", callback_data=f"today:{city}"),
         InlineKeyboardButton("Завтра", callback_data=f"tomorrow:{city}"),
     )
-    keyboard.add(InlineKeyboardButton("Неделя", callback_data=f"week:{city}"))
+    keyboard.add(
+        InlineKeyboardButton("Неделя", callback_data=f"week:{city}"),
+        InlineKeyboardButton("История", callback_data="history"),
+    )
 
     await bot.send_message(chat_id, "Выберите период прогноза:", reply_markup=keyboard)
+
 
 @dp.callback_query_handler(lambda c: c.data.startswith(('today:', 'tomorrow:', 'week:')))
 async def process_callback(callback_query: types.CallbackQuery):
@@ -125,14 +139,62 @@ async def process_callback(callback_query: types.CallbackQuery):
     await bot.send_message(callback_query.from_user.id, forecast_str)
 
 
+async def send_help_message(chat_id):
+    help_text = (
+        "Для того, чтобы получить информацию о погоде, введите название города.\n"
+        "Я предоставлю вам прогноз погоды на сегодня, завтра и неделю вперед.\n"
+        "Вы также сможете увидеть историю своих запросов."
+    )
+    await bot.send_message(chat_id, help_text)
+
+
 @dp.message_handler(commands=['start'])
 async def cmd_start(message: types.Message):
-    await message.reply("Привет! Я погодный бот. Введите название города, чтобы узнать прогноз погоды.")
+    start_text = (
+        "Привет! Я погодный бот. Я могу предоставить информацию о погоде "
+        "в разных городах, а также показать историю ваших запросов.\n"
+        "Если вам нужна помощь, нажмите на кнопку /help ниже."
+    )
+    keyboard = InlineKeyboardMarkup().add(InlineKeyboardButton("Помощь", callback_data="help"))
+    await bot.send_message(message.chat.id, start_text, reply_markup=keyboard)
+
+
+@dp.message_handler(commands=['help'])
+async def cmd_help(message: types.Message):
+    await send_help_message(message.chat.id)
+
+@dp.callback_query_handler(lambda c: c.data == "help")
+async def process_help(callback_query: types.CallbackQuery):
+    await bot.answer_callback_query(callback_query.id)
+    await send_help_message(callback_query.from_user.id)
+
 
 @dp.message_handler()
 async def process_city(message: types.Message):
     city = message.text
-    await send_forecast_buttons(message.chat.id, city)  # Выводим кнопки после ввода города
+    weather_data = await get_weather(city)
+
+    if not weather_data:
+        await message.reply(f"🚫 {city} не найден. Пожалуйста, проверьте название города и попробуйте еще раз.")
+        return
+
+    add_to_history(message.from_user.id, city)
+    await send_forecast_buttons(message.chat.id, city)
+
+
+@dp.callback_query_handler(lambda c: c.data == "history")
+async def process_history(callback_query: types.CallbackQuery):
+    user_id = callback_query.from_user.id
+    if user_id not in user_history or not user_history[user_id]:
+        history_text = "История пуста."
+    else:
+        history_text = "История запросов:\n"
+        for city, timestamp in user_history[user_id]:
+            history_text += f"{timestamp}: {city}\n"
+
+    await bot.answer_callback_query(callback_query.id)
+    await bot.send_message(user_id, history_text)
+
 
 @dp.callback_query_handler(lambda c: c.data.startswith(('today:', 'tomorrow:', 'week:')))
 async def process_callback(callback_query: types.CallbackQuery):
@@ -153,7 +215,6 @@ async def process_callback(callback_query: types.CallbackQuery):
     forecast_str = format_forecast(forecast_data, days)
     await bot.answer_callback_query(callback_query.id)
     await bot.send_message(callback_query.from_user.id, forecast_str)
-
 
 if __name__ == '__main__':
     from aiogram import executor
